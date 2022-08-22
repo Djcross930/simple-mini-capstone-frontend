df3627b98fb924de02a23d2653d41d2d4d78f7a0
Dan Cross <dancross96@gmail.com>
2022-08-22 13:15:55 -0500

    added fronted hardcoded create action for products

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,6 +16,18 @@ export default {
         console.log(response.data)
         this.products = response.data
       })
+    },
+    createProduct: function () {
+      var params = {
+        name: "Table",
+        price: 200,
+        description: "Put anything you want on this bad boy. You can even eat on it.",
+        image_url: "https://alacraterentals.com/wp-content/uploads/2018/02/ALaCrate-Rentals-Table-Harvest-ThreeQuarterView-Wood-Table-Rental-Weddings-Events-MadeInWisconsin-WEB-600x600-ALM2-600x600.jpg"
+      }
+      axios.post("http://localhost:3000/products.json", params).then(response => {
+        console.log(response.data)
+        this.products.push(response.data);
+      })
     }
   }
 };
@@ -23,6 +35,7 @@ export default {
 
 <template>
   <div class="home">
+    <button v-on:click="createProduct">Create Product</button>
     <div v-for="product in products">
       <p> {{ product.name }} </p>
       <p> ${{ product.price }} </p>
