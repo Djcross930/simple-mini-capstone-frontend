5e3ab7583484ad894e034c42f1005e23647bc9d2
Dan Cross <dancross96@gmail.com>
2022-08-22 12:54:43 -0500

    vue skeleton and added a basic index of all products from backend request

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function () {
+    this.indexProducts()
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then(response => {
+        console.log(response.data)
+        this.products = response.data
+      })
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="home">
+    <div v-for="product in products">
+      <p> {{ product.name }} </p>
+      <p> ${{ product.price }} </p>
+      <p> {{ product.description }} </p>
+      <img v-bind:src="product.image_url" />
+      <br />
+      <br />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
